694ad25107adab71e701ddd153afb2edcfd9de97
Antonio Hernandez <antonio.hernandez5815@alumnos.udg.mx>
2020-10-04 10:57:41 -0500

    prueba01

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("Hola Mundo")
+dia = 5
+mes = 1
+
+if  (31>= dia >=21 and mes ==3)or(1<= dia<=20 and mes==4):
+    print("Aries")
+if  (30>= dia >=21 and mes==4)or(1<= dia <=20 and mes==5):
+    print("Tauro")
+if  (31>= dia >=21 and mes==5)or(1<= dia <=21 and mes==6):
+    print("Geminis")
+if  (30>= dia >=22 and mes==6)or(1<= dia <=22 and mes==7):
+    print("Cancer")
+if  (31>= dia >=23 and mes==7)or(1<= dia <=22 and mes==8):
+    print("Leo")
+if  (31>= dia >=23 and mes==8)or(1<= dia <=22 and mes==9):
+    print("Virgo")
+if  (30>= dia >=23 and mes==9)or(1<= dia <=22 and mes==10):
+    print("Libra")
+if  (31>= dia >=23 and mes==10)or(1<= dia <=22 and mes==11):
+    print("Escorpio")
+if  (30>= dia >=23 and mes==11)or(1<= dia <=21 and mes==12):
+    print("Sagitario")
+if  (31>= dia >=22 and mes==12)or(1<= dia <=20 and mes==1):
+    print("Capricornio")
+if  (31>= dia >=21 and mes==1)or(1<= dia <=18 and mes==2):
+    print("Aquario")
+if  (29>= dia >=19 and mes==2)or(1<= dia <=20 and mes==3):
+    print("Piscis")
+    
